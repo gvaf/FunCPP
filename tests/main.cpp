f8918fd2d35ee6297cb49be3e778a3239c9ace84
George Vafiadis <gvaf@ntua.fusion-algorithms.com>
2011-11-30 13:18:12 +0200

    Added regular cmake configuration PCRE PCRECPP

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 #include <funcpp/string.h>
-#include <regex>
+#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <pcre.h>
+#include <pcrecpp.h>
 
 //-----------------------------------------------------------------
 void test_string_get_reverse()
@@ -64,10 +68,74 @@ void test_string_each_byte()
 
 //-----------------------------------------------------------------
 
+#define OVECCOUNT 30  /* should be a multiple of 3 */
+
 void test_reg_exp()
 {
+ pcre *re;
+ const char *error;
+ int erroffset;
+ int ovector[OVECCOUNT];
+ int rc;
 
-cout << endl;
+ char *regex = "^From: ([^@]+)@([^\r]+)";
+ char *data = "From: gvaf@example.com\r\n";
+
+ re = pcre_compile(
+  regex,     // the pattern
+  0,           // default options
+  &error,      // for error message
+  &erroffset,  // error offset
+  NULL);      // Default character table
+
+ cout << endl;
+
+ if (! re)
+ {
+   cout << "PCRE compilation failed at expression " <<  erroffset << ": " <<  error << endl;;
+    return;
+ }
+
+ rc = pcre_exec(
+  re,               // the compiled pattern
+  NULL,            // no extra data
+  data,           // the subject string
+  strlen(data),    // the length of the subject
+  0,               // start offset 0 in the subject
+  0,               // default options
+  ovector,         // output vector for substring information
+  OVECCOUNT);        // number of elements in the output
+
+
+ if(rc < 0)
+  {
+    switch(rc)
+     {
+        case PCRE_ERROR_NOMATCH:
+             printf("No match found in text\n");
+             break;
+        default:
+             printf("Match error %d\n", rc);
+             break;
+             return;
+     };
+  }
+
+
+ if(rc < 3)
+  {
+     printf("Match did not catch all the groups\n");
+     return;
+  }
+
+ /*ovector[0]..ovector[1] are the entire matched string*/
+ char *name_start = data + ovector[2];
+ int name_length = ovector[3] - ovector[2];
+ char *domain_start = data + ovector[4];
+ int domain_length = ovector[5] - ovector[4];
+
+ /* Finally, print the match */
+ printf("Mail from: %.*s domain: %.*s\n", name_length, name_start, domain_length, domain_start);
 
 }
 
@@ -78,7 +146,14 @@ int main()
   test_string_each_byte();
 //  test_string_reverse();
   test_string_get_reverse();
-//  test_reg_exp();
+  test_reg_exp();
+
+
+  //http://www.mushclient.com/pcre/pcrecpp.html
+  // ndex(substring [, offset]) → fixnum or nil
+  // index(regexp [, offset]) → fixnum or nil
+  pcrecpp::RE re("h.*o");
+  re.FullMatch("hello");
 
  return 0;
 }
